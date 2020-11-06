9245e57d90aea5c6dad7471fb1a6d05a34326b5b
Joewings-jw <ngugijoseph830@gmail.com>
2020-11-06 12:13:49 +0300

    configured routes

---
+++ src/app/routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from "@angular/router"
+import { Error404Component } from './errors/404-component'
+import { CreateEventComponent,
+        EventListResolver,
+        EventListComponent,
+        EventRouteActivator,
+        EventDetailComponent,  
+        CreateSessionComponent
+ } from './events/index'
+import { LoginComponent } from './user/login/login.component'
+import { ProfileComponent } from './user/profile/profile.component'
+
+
+export const AppRoutes: Routes = [
+    {path: 'events/new', component: CreateEventComponent},
+    {path: 'events', component: EventListComponent, resolve: {events:EventListResolver}},
+    {path: 'events/:id', component: EventDetailComponent, canActivate: [EventRouteActivator] },
+    {path: 'events/session/new', component: CreateSessionComponent},
+    {path: '404', component: Error404Component},
+    {path: '', redirectTo: '/events', pathMatch: 'full'},
+    {path: 'user/profile', component: ProfileComponent},
+    {path: 'user/login', component: LoginComponent}
+   
+   
+]
