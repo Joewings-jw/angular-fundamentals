466be953b332085f31add0be55e62278ec1bcca7
Joewings-jw <ngugijoseph830@gmail.com>
2020-11-06 12:19:05 +0300

    modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule }from '@angular/forms'
+ 
 import { AppComponent } from './app.component';
+import {
+  EventListComponent,
+  EventThumbnailComponent,
+  EventDetailComponent,
+  CreateEventComponent,
+  CreateSessionComponent,
+  SessionListComponent
+} from './events/index'
+
+import { NavbarComponent } from './nav/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutes } from './routes';
+import { Error404Component } from './errors/404-component';
+import { UserModule } from "./user/user-module/user.module";
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventListComponent,
+    EventThumbnailComponent,
+    NavbarComponent,
+    EventDetailComponent,
+    CreateEventComponent,
+    Error404Component,
+    CreateSessionComponent,
+    SessionListComponent,
+  
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(AppRoutes),
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
