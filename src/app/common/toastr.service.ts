f79100d6b0daa5b9e9baa18f29402a161a15eb80
Joewings-jw <ngugijoseph830@gmail.com>
2020-11-10 19:54:17 +0300

    created a local reference variable for use by the sibling nodes for rerunning custom location validator

--- src/app/common/toastr.service.ts
+++ src/app/common/toastr.service.ts
@@ -1,27 +1,10 @@
-import { Injectable } from '@angular/core';
+import { InjectionToken } from '@angular/core';
 
+export let TOASTR_TOKEN = new InjectionToken<Toastr>('toastr');
 
-declare let toastr:any;
-@Injectable({
-  providedIn: 'root'
-})
-export class ToastrService {
-
-  success(message: string, title?: string) {
-    toastr.success(message, title)
-  }
-  info(message: string, title?: string) {
-    toastr.info(message, title)
-  }
-  warning(message: string, title?: string) {
-    toastr.warning(message, title)
-  }
-  errror(message: string, title?: string) {
-    toastr.error(message, title)
-  }
-  
-
-
-
-
+export interface Toastr {
+  success(msg: string, title?: string): void;
+  info(msg: string, title?: string): void;
+  warning(msg: string, title?: string): void;
+  error(msg: string, title?: string): void;
 }
