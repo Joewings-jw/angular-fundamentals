7f2ae6fd22b666ed8265f9c7fa24f18ab0908286
Joewings-jw <ngugijoseph830@gmail.com>
2020-11-10 20:17:38 +0300

    subscribed to search results on a mock-up delay to create an observable

--- src/app/nav/navbar/navbar.component.ts
+++ src/app/nav/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EventsService, ISession } from 'src/app/events';
 import { AuthService } from 'src/app/user/auth.service';
 
 @Component({
@@ -7,10 +9,24 @@ import { AuthService } from 'src/app/user/auth.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  search_term:string = ""
+  found_sessions: ISession[]
+  
 
-  constructor(public auth: AuthService) { }
+  constructor(public auth: AuthService,
+    private router: Router,
+    private event_service: EventsService) { }
 
   ngOnInit() {
+  }
+
+  search_session(search_term){
+    this.event_service.search_sessions(search_term).subscribe(
+      sessions => {
+        this.found_sessions = sessions;
+      }
+    )
+
   }
 
 }
