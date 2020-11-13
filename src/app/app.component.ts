63a619145b1ef40c91e1102801207ef4cde00fa6
Joewings-jw <ngugijoseph830@gmail.com>
2020-11-13 09:58:13 +0300

    subscribed to auth services for checking user identity status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './user/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-fundamentals';
+  constructor(private auth: AuthService){}
+
+  ngOnInit(){
+    this.auth.check_auth_status()
+  }
 }
